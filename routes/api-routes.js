89428de340426581b8516c69e3571f0db423f4bd
TDWalsh <thomas.walsh736@gmail.com>
2020-10-23 19:19:39 -0400

    added get route for testing to find all workouts

---
+++ routes/api-routes.js
@@ -0,0 +1,24 @@
+
+const express = require("express");
+const router = express.Router();
+
+const db = require("../models");
+
+// Find all workouts
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find()
+    .then((workouts) => {
+      res.json(workouts);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "Failed to retrieve workouts.",
+      });
+    });
+});
+
+
+module.exports = router;
