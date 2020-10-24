c2b381f3050edc1a27162938e32c37e131c60a74
TDWalsh <thomas.walsh736@gmail.com>
2020-10-23 20:09:31 -0400

    added api route for the stats page

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,11 +4,10 @@ const router = express.Router();
 
 const db = require("../models");
 
-// Find all workouts
 router.get("/api/workouts", (req, res) => {
   db.Workout.find()
-    .then((workouts) => {
-      res.json(workouts);
+    .then((workout) => {
+      res.json(workout);
     })
     .catch((err) => {
       console.log(err);
@@ -34,6 +33,43 @@ router.post("/api/workouts", (req, res) => {
         });
       });
   });
+
+  router.put("/api/workouts/:id", (req, res) => {
+      db.Workout.findByIdAndUpdate(req.params.id, {
+        $push: { exercises: req.body },
+      })
+        .then((updatedWorkout) => {
+          res.json(updatedWorkout);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.json({
+            error: true,
+            data: null,
+            message: "Failed to add to workout.",
+          });
+        });
+  });
+
+
+  router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .limit(10)
+      .sort({day: -1})
+      .then((workoutsInRange) => {
+        res.json(workoutsInRange);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: true,
+          data: null,
+          message: "Failed to retrieve workouts in range.",
+        });
+      });
+  });
+
+ 
 
 
 module.exports = router;
